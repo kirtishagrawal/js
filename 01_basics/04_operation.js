84e63f8a194dad167bde4af35d0cfa8454ca9718
kirtishagrawal <agrawalkirtishshu@gmail.com>
2024-08-04 09:00:18 +0000

    operations

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,18 @@
+// let number = 3
+// let negnumber = -number
+
+// console.log(negnumber)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2**3)               // to find power
+// console.log(5%3)                // to find number, divisior in left, divident in right
+
+// let str1 = "kirtish"
+// let str2 = " agrawal"
+
+// console.log(str1 + str2)            // addition of string is possible but any other operation is not
+
+
