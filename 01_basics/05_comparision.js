7ae385ca8880caf102b305839c76c401e5994bcc
kirtishagrawal <agrawalkirtishshu@gmail.com>
2024-08-04 09:11:15 +0000

    comparision

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,11 @@
+// console.log(2 > 2)
+// console.log(2 == 2)
+// console.log(2 < 2)
+// console.log(2 <= 2)
+// console.log(2 >= 2)
+// console.log(2 != 2)
+
+//before comparision make sure the data types are same
+
+console.log("2" == 2)       // true because datatype is not checked, equality check
+console.log("2" === 2)      // flase because datatype is checked, stricked check
