1c9a04646ef2a4ce7b08b3cb6c2e07bec068ac58
kirtishagrawal <agrawalkirtishshu@gmail.com>
2024-08-04 08:25:41 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //use all JS code as newer version//
+
+// alert (3 + 3) //we are using nodeJS not browser//
+
+let name = "Kirtish"
+let age = 21
+let isLoggedIn = true
+let address
+let work = null
+
+console.log(typeof name)            //string
+console.log(typeof age)             //number
+console.log(typeof isLoggedIn)      //boolean
+console.log(typeof address)         //undefined
+console.log(typeof work)            //object
+
+//highest numer value is 2 to the power of 53, more big number is assigned by bigint//
+// to assign string value use ""
+
